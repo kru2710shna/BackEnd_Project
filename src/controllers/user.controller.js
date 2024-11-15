7878d3234f6d16b300aa4cf17991e9ff592caebc
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-11-14 22:30:57 -0800

    Get Watch History EndPoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -412,4 +412,61 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAcessToken, ChnageCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile }
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+
+    const user = await User.aggregate(
+        [
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(req.user._id)
+                }
+            },
+            {
+                $lookup: {
+                    from: "Video",
+                    localField: "watchHistory",
+                    foreignField: "_id",
+                    as: "watchHistory",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "Users",
+                                localField: "owner",
+                                foreignField: "_id",
+                                as: "owner",
+                                pipeline: [
+                                    {
+                                        $project: {
+                                            fullname: 1,
+                                            username: 1,
+                                            avatar: 1
+
+                                        }
+                                    },
+                                    {
+                                        $addFields: {
+                                            owner: {
+                                                $first: "$owner"
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+
+                }
+            }
+        ]
+    )
+    return response
+        .status(200,
+            user[0].watchHistory,
+            "Watch History Fetched"
+        )
+
+})
+
+
+export { registerUser, loginUser, logoutUser, refreshAcessToken, ChnageCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory }
